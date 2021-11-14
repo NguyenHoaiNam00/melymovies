fabe7fa5b1e67fc2e80dde57893df710a1447870
NguyenHoaiNam00 <hoainamnguyennhn02@gmail.com>
2021-11-14 20:53:33 +0700

    Push final project melymovies by Nguyen Hoai Nam

---
+++ resources/views/user/about.blade.php
@@ -0,0 +1,33 @@
+@extends('user.master')
+@section('title','Giới Thiệu Về Chúng Tôi - MeLyMovies')
+@section('content')
+<div class="faq">
+    <h4 class="latest-text w3_faq_latest_text w3_latest_text">Giới Thiệu</h4>
+    <div class="container">
+        <div class="agileits-news-top">
+            <ol class="breadcrumb">
+                <li><a href="{{ route('user.index') }}">Trang Chủ</a></li>
+                <li class="active">Giới Thiệu</li>
+            </ol>
+            <div>
+                <h4>Xem phim hay miễn phí chất lượng cao. Mely Movies có nhiều thể loại phim phong phú, đặc sắc, nhiều bộ phim hay nhất - mới nhất.
+                    với giao diện trực quan, thuận tiện, tốc độ tải nhanh, ít quảng cáo hứa hẹn sẽ đem lại những trải nghiệm tốt nhất cho người dùng.<br><br>
+                    Ngày nay phim ảnh là một trong những lĩnh vực mang đến doanh thu khổng lồ.Không phải ai cũng có nhu cầu và sở thích xem phim tại rạp.<br><br>
+                    Với niềm đam mê về phim ảnh, chúng tôi những người xây dựng nên Website này mong muốn đưa tới cho
+                    mọi người những bộ phim hay nhất trên thị trường hiện tại, mới nhất và cập nhật liên tục. Chúng tôi
+                    cam đoan tất cả phim trên Website của chúng tôi đều có chất lượng tốt nhất để phục vụ cho mọi người.<br><br>
+                    Đến với MeLy Movies bạn có thể xem phim trực tuyến có trả phí với đầy đủ tính năng ưu việt dành cho những người
+                    không có thời gian, thiếu thốn về kinh tế, không am hiểu về xem phim tại rạp hay đơn giản là muốn xem phim có bản quyền.<br><br></h4>
+            </div>
+        </div>
+    </div>
+    <h4 class="latest-text w3_faq_latest_text w3_latest_text">Liên Hệ</h4>
+    <div class="container">
+        <h4 class="pt-1"><b>Quản trị viên: </b>Nguyễn Hoài Nam</h4>
+        <h4 class="pt-1"><b>Số điện thoại: </b>0848723439</h4>
+        <h4><b>Email: </b>hoainamnguyennhn02@gmail.com</h4>
+        <h4><b>Địa chỉ: </b>Bình Dương, Việt Nam</h4>
+
+    </div>
+</div>
+@endsection
